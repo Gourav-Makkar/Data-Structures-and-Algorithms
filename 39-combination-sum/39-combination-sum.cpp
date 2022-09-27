a59e02d88f4b140a4a128683def9033b82250eda
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-09-27 18:23:28 +0530

    Time: 221 ms (12.91%), Space: 114.5 MB (5.29%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -3,22 +3,31 @@ public:
     
     void helper(vector<int>ip,vector<int>op,int cs,int idx,int target,vector<vector<int>>&ans)
     {
-        if(cs>target)
+        if(cs>target || idx==ip.size())
             return;
         if(cs==target)
         {
             ans.push_back(op);
             return;
         }
-        for(int i=idx;i<ip.size();i++)
-        {
-            cs+=ip[i];
-            op.push_back(ip[i]);
-            helper(ip,op,cs,i,target,ans);
+//         for(int i=idx;i<ip.size();i++)
+//         {
+//             cs+=ip[i];
+//             op.push_back(ip[i]);
+//             helper(ip,op,cs,i,target,ans);
             
-            cs-=ip[i];
-            op.pop_back();
-        }
+//             cs-=ip[i];
+//             op.pop_back();
+//         }
+        
+        cs+=ip[idx];
+        op.push_back(ip[idx]);
+        helper(ip,op,cs,idx,target,ans);
+        
+        cs-=ip[idx];
+        op.pop_back();
+        helper(ip,op,cs,idx+1,target,ans);
+        
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
