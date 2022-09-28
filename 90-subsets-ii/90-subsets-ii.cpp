1a940b659e73f4afdebb2571d0c80358dea67b80
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-09-28 20:17:11 +0530

    Time: 8 ms (41.88%), Space: 7.8 MB (47.14%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -4,8 +4,6 @@ public:
     void helper(vector<int>ip,vector<int>op,int idx,vector<vector<int>>&ans)
     {
         ans.push_back(op);
-        if(idx>=ip.size())
-            return;
         for(int i=idx;i<ip.size();i++)
         {
             if(i>idx && ip[i]==ip[i-1])
