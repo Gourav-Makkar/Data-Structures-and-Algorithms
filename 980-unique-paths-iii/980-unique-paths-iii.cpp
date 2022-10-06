81b51768b1b52d8448407a8f47adf7803df01a0f
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-10-06 18:22:59 +0530

    Time: 406 ms (5.04%), Space: 177 MB (5.35%) - LeetHub

--- 980-unique-paths-iii/980-unique-paths-iii.cpp
+++ 980-unique-paths-iii/980-unique-paths-iii.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     int ans=0;
     
-    void helper(vector<vector<int>>grid,int cr,int cc,vector<vector<int>>&visited,int ct)
+    void helper(vector<vector<int>>grid,int cr,int cc,int ct)
     {
-        if(cr<0 || cr>=grid.size() || cc>=grid[0].size() || cc<0)
+        if(cr<0 || cr>=grid.size() || cc>=grid[0].size() || cc<0 || grid[cr][cc]==-1)
             return;
-        if(visited[cr][cc]==1 || grid[cr][cc]==-1)
-            return;
+        // if(visited[cr][cc]==1 || grid[cr][cc]==-1)
+        //     return;
         if(grid[cr][cc]==2 && ct==1)
         {
             ans++;
             return;
         }
-        visited[cr][cc]=1;
+        grid[cr][cc]=-1;
         
-        helper(grid,cr,cc+1,visited,ct-1);
-        helper(grid,cr+1,cc,visited,ct-1);
-        helper(grid,cr,cc-1,visited,ct-1);
-        helper(grid,cr-1,cc,visited,ct-1);
+        helper(grid,cr,cc+1,ct-1);
+        helper(grid,cr+1,cc,ct-1);
+        helper(grid,cr,cc-1,ct-1);
+        helper(grid,cr-1,cc,ct-1);
         
-        visited[cr][cc]=0;
+        grid[cr][cc]=0;
     }
     
     int uniquePathsIII(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>visited(n,vector<int> (m,0));
+        // vector<vector<int>>visited(n,vector<int> (m,0));
         
         int ct=0,stRow,stCol;
         for(int i=0;i<n;i++)
@@ -44,7 +44,7 @@ public:
         }
         ct=ct+2;
         
-        helper(grid,stRow,stCol,visited,ct);
+        helper(grid,stRow,stCol,ct);
         
         return ans;
     }
