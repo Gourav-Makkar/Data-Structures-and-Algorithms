84423b72a69f19ddea3c0fe8deda4fe85318a522
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-10-10 19:55:12 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (31.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,8 +3,8 @@ public:
     
     int helper(int n,vector<int>&dp)
     {
-        if(n==1 || n==2)
-            return n;
+        if(n==0 || n==1)
+            return 1;
         if(dp[n]!=-1)
             return dp[n];
         return (dp[n]=helper(n-1,dp)+helper(n-2,dp));
