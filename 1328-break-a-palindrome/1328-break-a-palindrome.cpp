2ce4f3157713d3b255934f4fe534de4ccfda3dc8
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-10-10 18:27:50 +0530

    Time: 2 ms (51.67%), Space: 6.2 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
      
-    bool isPalindrome(string s)
-    {
-        int i=0,j=s.length()-1;
-        while(i<j)
-        {
-            if(s[i]!=s[j])
-                return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
+    // bool isPalindrome(string s)
+    // {
+    //     int i=0,j=s.length()-1;
+    //     while(i<j)
+    //     {
+    //         if(s[i]!=s[j])
+    //             return false;
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
     
     string breakPalindrome(string palindrome) {
         int n=palindrome.length();
