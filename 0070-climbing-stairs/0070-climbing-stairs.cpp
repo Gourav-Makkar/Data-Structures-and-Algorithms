1558c1a42190e9bba0f3b37331a88155d5d5ac78
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-10-10 20:01:40 +0530

    Time: 3 ms (36.29%), Space: 5.8 MB (83.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
+//     MEMOIZATION
+    // int helper(int n,vector<int>&dp)
+    // {
+    //     if(n==0 || n==1)
+    //         return 1;
+    //     if(dp[n]!=-1)
+    //         return dp[n];
+    //     return (dp[n]=helper(n-1,dp)+helper(n-2,dp));
+    // }
     
-    int helper(int n,vector<int>&dp)
-    {
-        if(n==0 || n==1)
-            return 1;
-        if(dp[n]!=-1)
-            return dp[n];
-        return (dp[n]=helper(n-1,dp)+helper(n-2,dp));
-    }
-    
+//     TABULATION
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return helper(n,dp);
+        if(n==1)
+            return 1;
+        int prev2=1,prev=1;
+        for(int i=2;i<=n;i++)
+        {
+            int cur=prev2+prev;
+            prev2=prev;
+            prev=cur;
+        }
+        return prev;  
     }
 };
