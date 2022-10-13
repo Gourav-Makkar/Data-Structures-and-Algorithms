2ca87bc55a77994a9ede7bb645a92fea69ce4125
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-10-13 18:36:38 +0530

    Time: 0 ms (100.00%), Space: 6 MB (67.14%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -22,28 +22,55 @@ public:
     int uniquePaths(int m, int n) {
         
         // int dr=m-1,dc=n-1;
-        vector<vector<int>>dp(m,vector<int>(n,-1));
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
         // int ans=0;
         // ans+=helper(dr,dc,dp);
         // return ans;
-    
+   
+//       TABULATION  
+       //  for(int i=0;i<m;i++)
+       //  {
+       //      for(int j=0;j<n;j++)
+       //      {
+       //          if(i==0 && j==0)
+       //          {
+       //              dp[i][j]=1;  //BASE CASE
+       //              continue;
+       //          }
+       //          int up=0,left=0;
+       //          if(i>0)
+       //              up=dp[i-1][j];
+       //          if(j>0)
+       //              left=dp[i][j-1];
+       //          dp[i][j]=up+left;
+       //      }
+       //  }
+       // return dp[m-1][n-1];
+        
+        
+//         SPACE OPTIMIZED
+        
+        vector<int>dp(n,0);
         for(int i=0;i<m;i++)
         {
+            vector<int>temp(n,0);
             for(int j=0;j<n;j++)
             {
                 if(i==0 && j==0)
                 {
-                    dp[i][j]=1;
+                    temp[j]=1;
                     continue;
                 }
-                int up=0,left=0;
-                if(i>0)
-                    up=dp[i-1][j];
+                int up=dp[j];
+                int left=0;
                 if(j>0)
-                    left=dp[i][j-1];
-                dp[i][j]=up+left;
+                    left=temp[j-1];
+
+                temp[j]=up+left;
             }
+            dp=temp;
         }
-       return dp[m-1][n-1];
+        return dp[n-1];
+        
     }
 };
