6f9cfbade2b185cfa4beb76830a371fb7ebca979
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-10-13 18:22:40 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (49.50%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -26,20 +26,22 @@ public:
         // int ans=0;
         // ans+=helper(dr,dc,dp);
         // return ans;
-        
+    
         for(int i=0;i<m;i++)
         {
-            dp[i][0]=1;
-        }
-        for(int i=0;i<n;i++)
-        {
-            dp[0][i]=1;
-        }
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
+            for(int j=0;j<n;j++)
             {
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                if(i==0 && j==0)
+                {
+                    dp[i][j]=1;
+                    continue;
+                }
+                int up=0,left=0;
+                if(i>0)
+                    up=dp[i-1][j];
+                if(j>0)
+                    left=dp[i][j-1];
+                dp[i][j]=up+left;
             }
         }
        return dp[m-1][n-1];
