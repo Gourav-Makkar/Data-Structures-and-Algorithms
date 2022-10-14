f49adc068108de9eca77241a313389ab4c54d6b2
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-10-14 20:08:36 +0530

    Time: 12 ms (50.85%), Space: 9 MB (51.85%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -21,24 +21,44 @@ public:
 //     TABULATION
     int minimumTotal(vector<vector<int>>& triangle) {
        int n=triangle.size();
-       vector<vector<int>>dp(n,vector<int>(n,-1));
-       // return helper(triangle,0,0,n,dp);
+//        vector<vector<int>>dp(n,vector<int>(n,-1));
+//        // return helper(triangle,0,0,n,dp);
         
-       for(int i=n-1;i>=0;i--)
-       {
-           for(int j=i;j>=0;j--)
-           {
-               if(i==n-1)
-               {
-                   dp[i][j]=triangle[i][j];
-                   continue;
-               }
-               int down=triangle[i][j]+ dp[i+1][j];
-               int diag=triangle[i][j]+dp[i+1][j+1];
+//        for(int i=n-1;i>=0;i--)
+//        {
+//            for(int j=i;j>=0;j--)
+//            {
+//                if(i==n-1)
+//                {
+//                    dp[i][j]=triangle[i][j];
+//                    continue;
+//                }
+//                int down=triangle[i][j]+ dp[i+1][j];
+//                int diag=triangle[i][j]+dp[i+1][j+1];
                
-               dp[i][j]=min(down,diag);
-           }
-       }
-    return dp[0][0];
+//                dp[i][j]=min(down,diag);
+//            }
+//        }
+//     return dp[0][0];
+        
+//         SPACE OPTIMIZATION
+        vector<int>dp(n);
+        for(int i=0;i<n;i++)
+        {
+            dp[i]=triangle[n-1][i];
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            vector<int>curr(i+1);
+            for(int j=i;j>=0;j--)
+            {
+                int down=triangle[i][j]+dp[j];
+                int diag=triangle[i][j]+dp[j+1];
+                
+                curr[j]=min(down,diag);
+            }
+            dp=curr;
+        }
+        return dp[0];
     }
 };
