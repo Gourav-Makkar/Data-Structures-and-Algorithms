3daa3ce8aeeb40e3ee6e2b4942b3b9aeaba64441
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-10-17 13:47:05 +0530

    Time: 35 ms (32.97%), Space: 10.3 MB (27.82%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     
-    int helper(vector<vector<int>>&matrix,int i,int j,vector<vector<int>>&dp)
-    {
-        if(j<0 || j>=matrix[0].size())
-            return 1e9;
-        if(i==matrix.size()-1)
-            return matrix[i][j];
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int down=matrix[i][j]+helper(matrix,i+1,j,dp);
-        int diagLeft=matrix[i][j]+helper(matrix,i+1,j-1,dp);
-        int diagRight=matrix[i][j]+helper(matrix,i+1,j+1,dp);
+//     MEMOIZATION
+    
+//     int helper(vector<vector<int>>&matrix,int i,int j,vector<vector<int>>&dp)
+//     {
+//         if(j<0 || j>=matrix[0].size())
+//             return 1e9;
+//         if(i==matrix.size()-1)
+//             return matrix[i][j];
+//         if(dp[i][j]!=-1)
+//             return dp[i][j];
+//         int down=matrix[i][j]+helper(matrix,i+1,j,dp);
+//         int diagLeft=matrix[i][j]+helper(matrix,i+1,j-1,dp);
+//         int diagRight=matrix[i][j]+helper(matrix,i+1,j+1,dp);
         
-        return dp[i][j]=min(down,min(diagLeft,diagRight));
-    }
+//         return dp[i][j]=min(down,min(diagLeft,diagRight));
+//     }
     
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int r=matrix.size();
@@ -22,9 +24,33 @@ public:
         int ans=INT_MAX;
         
         vector<vector<int>>dp(r,vector<int>(c,-1));
+        // for(int i=0;i<c;i++)
+        // {
+        //     ans=min(ans,helper(matrix,0,i,dp));
+        // }
+        // return ans;
+        for(int j=0;j<c;j++)
+        {
+            dp[r-1][j]=matrix[r-1][j];
+        }
+       for(int i=r-2;i>=0;i--)
+        {
+            for(int j=0;j<c;j++)
+            {
+                int down=0,diagleft=1e9,diagright=1e9;
+                down=matrix[i][j]+dp[i+1][j];
+                if(j>0)
+                    diagleft=matrix[i][j]+dp[i+1][j-1];
+                if(j<c-1)
+                    diagright=matrix[i][j]+dp[i+1][j+1];
+                
+                dp[i][j]=min(down,min(diagleft,diagright));
+            }
+        }
+        
         for(int i=0;i<c;i++)
         {
-            ans=min(ans,helper(matrix,0,i,dp));
+            ans=min(ans,dp[0][i]);
         }
         return ans;
     }
