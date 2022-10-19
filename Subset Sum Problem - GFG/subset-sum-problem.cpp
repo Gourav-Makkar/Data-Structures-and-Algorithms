386fce87e65d55035d138a9914446cb7e6185efc
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-10-19 20:30:06 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -80,8 +80,8 @@ public:
         
         vector<bool>prev(sum+1,false);
         prev[0]=true;
-        // prev[arr[0]]=true;
-        for(int i=0;i<n;i++)
+        prev[arr[0]]=true;
+        for(int i=1;i<n;i++)
         {
             vector<bool>curr(sum+1,false);
             curr[0]=true;
