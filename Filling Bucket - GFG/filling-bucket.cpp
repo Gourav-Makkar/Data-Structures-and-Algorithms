9728f239a4f1d6d3df8f451b7f9b6395c1db8984
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-10-22 11:52:06 +0530

    Added solution - LeetHub

--- Filling Bucket - GFG/filling-bucket.cpp
+++ Filling Bucket - GFG/filling-bucket.cpp
@@ -6,33 +6,47 @@ using namespace std;
 class Solution {
   public:
   
-    int helper(int N,vector<int>&dp)
-    {
-        if(N==0)
-          return 1;
-        if(dp[N]!=-1)
-          return dp[N];
-        int first=(helper(N-1,dp))%100000000;
-        int second=0;
-        if(N>1)
-          second=(helper(N-2,dp))%100000000;
-        return dp[N]=(first+second)%100000000;
-    }
+//   MEMOIZATION
+  
+    // int helper(int N,vector<int>&dp)
+    // {
+    //     if(N==0)
+    //       return 1;
+    //     if(dp[N]!=-1)
+    //       return dp[N];
+    //     int first=(helper(N-1,dp))%100000000;
+    //     int second=0;
+    //     if(N>1)
+    //       second=(helper(N-2,dp))%100000000;
+    //     return dp[N]=(first+second)%100000000;
+    // }
     
+    // TABULATION
     int fillingBucket(int N) {
         // code here
-        vector<int>dp(N+1,0);
+        // vector<int>dp(N+1,0);
         // return helper(N,dp);
-        dp[0]=1;
+        // dp[0]=1;
+        // for(int i=1;i<=N;i++)
+        // {
+        //   int first=dp[i-1]%100000000;
+        //   int second=0;
+        //   if(i>1)
+        //     second=dp[i-2]%100000000;
+        //   dp[i]=(first+second)%100000000;
+        // }
+        // return dp[N];
+        
+        // SPACE OPTIMIZED
+        
+        int prev2=0,prev=1;
         for(int i=1;i<=N;i++)
         {
-          int first=dp[i-1]%100000000;
-          int second=0;
-          if(i>1)
-            second=dp[i-2]%100000000;
-          dp[i]=(first+second)%100000000;
+            int curr=(prev+prev2)%100000000;
+            prev2=prev;
+            prev=curr;
         }
-        return dp[N];
+        return prev;
     }
 };
 
