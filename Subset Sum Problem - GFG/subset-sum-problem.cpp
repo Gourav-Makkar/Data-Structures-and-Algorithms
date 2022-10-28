143cbb27e6ead133d0b51293650a8337492475a0
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-10-28 17:40:16 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -10,93 +10,26 @@ using namespace std;
 class Solution{   
 public:
 
-// START FROM 0
-
-    // bool helper(vector<int>arr,int idx,int target,int n,vector<vector<int>>&dp)
-    // {
-    //     if(target==0)
-    //       return true;
+    bool helper(vector<int>&arr,int idx,int target,vector<vector<int>>&dp)
+    {
+        if(target==0)
+          return true;
+        if(idx==0)
+          return arr[0]==target;
           
-    //     if(idx==n)
-    //       return false;
-         
-    //     if(dp[idx][target]!=-1)
-    //       return dp[idx][target];
-          
-    //     if(helper(arr,idx+1,target-arr[idx],n,dp) || helper(arr,idx+1,target,n,dp))
-    //       return dp[idx][target]=true;
-          
-    //     return dp[idx][target]=false;
-    // }
+        if(dp[idx][target]!=-1)
+          return dp[idx][target];
+        bool pick=helper(arr,idx-1,target-arr[idx],dp);
+        bool notPick=helper(arr,idx-1,target,dp);
+        
+        return dp[idx][target]=pick||notPick;
+    }
     
-    
-    // START FROM BACK
-    
-    //  bool helper(vector<int>arr,int idx,int target,vector<vector<int>>&dp)
-    // {
-    //     if(target==0)
-    //       return true;
-          
-    //     if(idx==0)
-    //       return arr[idx]==target;
-          
-    //     if(dp[idx][target]!=-1)
-    //       return dp[idx][target];
-          
-    //     bool notTake=helper(arr,idx-1,target,dp);
-    //     bool take=false;
-    //     if(target>=arr[idx])
-    //       take=helper(arr,idx-1,target-arr[idx],dp);
-    //     return dp[idx][target]= notTake || take;
-    // }
-
-//   TABULATION
     bool isSubsetSum(vector<int>arr, int sum){
         // code here 
         int n=arr.size();
-        // vector<vector<bool>>dp(n,vector<bool>(sum+1,false));
-      
-        // // return helper(arr,n-1,sum,dp);
-        
-        // for(int i=0;i<n;i++)
-        //   dp[i][0]=true;
-          
-        // dp[0][arr[0]]=true;
-        
-        // for(int i=1;i<n;i++)
-        // {
-        //     for(int j=1;j<=sum;j++)
-        //     {
-        //         bool notTake=dp[i-1][j];
-        //         bool take=false;
-        //         if(j>=arr[i])
-        //           take=dp[i-1][j-arr[i]];
-        //         dp[i][j]=take || notTake; 
-        //     }
-        // }
-        // return dp[n-1][sum];
-        
-        // SPACE OPTIMIZED
-        
-        vector<bool>prev(sum+1,false);
-        prev[0]=true;
-        prev[arr[0]]=true;
-        for(int i=1;i<n;i++)
-        {
-            vector<bool>curr(sum+1,false);
-            curr[0]=true;
-            for(int target=1;target<=sum;target++)
-            {
-                bool notTake=prev[target];
-                bool take=false;
-                if(target>=arr[i])
-                 take=prev[target-arr[i]];
-                curr[target]=take||notTake;
-            }
-            prev=curr;
-        }
-        return prev[sum];
-        
+        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        return helper(arr,n-1,sum,dp);
     }
 };
 
