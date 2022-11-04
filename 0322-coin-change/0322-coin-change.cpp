83f135569c04eef5744ae8e21bcd5d5c559bfe0f
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-11-04 20:31:57 +0530

    Time: 320 ms (13.65%), Space: 45.2 MB (25.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -25,11 +25,33 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=helper(coins,amount,n-1,dp);
-        if(ans==1e9)
-            return -1;
-        return ans;
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        // int ans=helper(coins,amount,n-1,dp);
+        // if(ans==1e9)
+        //     return -1;
+        // return ans;
         
+        for(int i=1;i<=amount;i++)
+        {
+            if(i%coins[0]==0)
+                dp[0][i]=i/coins[0];
+            else
+                dp[0][i]=1e9;
+        }
+        
+        for(int idx=1;idx<n;idx++)
+        {
+            for(int tar=0;tar<=amount;tar++)
+            {
+                int notTake=0+dp[idx-1][tar];
+                int take=1e9;
+                if(tar>=coins[idx])
+                    take=1+dp[idx][tar-coins[idx]];
+                dp[idx][tar]=min(notTake,take);
+            }
+        }
+        if(dp[n-1][amount]==1e9)
+            return -1;
+        return dp[n-1][amount];
     }
 };
