f1295e00a956d47047cb2dd2cdfb39e22d0c3ca8
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-11-04 10:57:10 +0530

    Time: 15 ms (55.02%), Space: 7.8 MB (37.49%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -4,7 +4,7 @@ public:
         string v="";
         for(int i=0;i<s.length();i++)
         {
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' ||                  s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
                 v.push_back(s[i]);
         }
         reverse(v.begin(),v.end());
@@ -12,7 +12,7 @@ public:
         int idx=0;
         for(int i=0;i<s.length();i++)
         {
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' ||                  s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
             {
                 s[i]=v[idx];
                 idx++;
