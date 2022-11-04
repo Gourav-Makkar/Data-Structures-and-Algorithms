41e18fa6ada61916ead57afad673f23ef8acbe9d
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-11-04 17:38:48 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -10,16 +10,21 @@ class Solution
     
     int helper(int wt[],int val[],int n,int w,int idx,vector<vector<int>>&dp)
     {
-        if(w==0 || idx==n)
+        if(w==0)
           return 0;
-        
+        if(idx==n-1)
+        {
+            if(w>=wt[idx])
+              return val[idx];
+            return 0;
+        }
         if(dp[idx][w]!=-1)
           return dp[idx][w];
         
         int notPick=0+helper(wt,val,n,w,idx+1,dp);
         int pick=0;
         if(w>=wt[idx])
-           pick=val[idx]+helper(wt,val,n,w-wt[idx],idx+1,dp);
+          pick=val[idx]+helper(wt,val,n,w-wt[idx],idx+1,dp);
            
         return dp[idx][w]=max(pick,notPick);
     }
@@ -29,7 +34,18 @@ class Solution
     { 
        // Your code here
        vector<vector<int>>dp(n,vector<int>(W+1,-1));
-       return helper(wt,val,n,W,0,dp);
+      return helper(wt,val,n,W,0,dp);
+        
+    //   for(int i=0;i<n;i++)
+    //      dp[i][0]=0;
+       
+    //   for(int idx=n-1;idx>=0;idx--)
+    //   {
+    //       for(int w=W;w>=1;w--)
+    //       {
+               
+    //       }
+    //   }
     }
 };
 
