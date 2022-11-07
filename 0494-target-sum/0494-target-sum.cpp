737db5634a17be9065a008eed1cafc30cdb353c7
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-11-08 01:06:46 +0530

    Time: 711 ms (25.53%), Space: 8.9 MB (98.92%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     
-    int helper(vector<int>&nums,int target,int idx,int cs)
+    int helper(vector<int>&nums,int target,int idx)
     {
-        if(idx==nums.size())
+        if(idx==0)
         {
-            if(cs==target)
+            if(target==0 && nums[0]==0)
+                return 2;
+            if(target==0 || nums[0]==target)
                 return 1;
             return 0;
         }
-        
-        int p=helper(nums,target,idx+1,cs+nums[idx]);
-        int m=helper(nums,target,idx+1,cs-nums[idx]);
-        
-        return (p+m);
+        int nt=helper(nums,target,idx-1);
+        int t=0;
+        if(nums[idx]<=target)
+            t=helper(nums,target-nums[idx],idx-1);
+        return t+nt;
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+            sum+=nums[i];
+        if(sum<target || (sum-target)%2!=0)
+            return 0;
+        int tar=(sum-target)/2;
         
-        return helper(nums,target,0,0);
+        return helper(nums,tar,n-1);
     }
 };
