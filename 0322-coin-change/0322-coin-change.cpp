b2761e70620508c91f15628370d63a8c8418ef66
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-11-08 00:48:26 +0530

    Time: 129 ms (72.81%), Space: 45.6 MB (22.48%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -5,53 +5,25 @@ public:
     {
         if(idx==0)
         {
-           if(amount%coins[0]==0)
-               return amount/coins[0];
-           else
-               return 1e9;
+            if(amount%coins[0]==0)
+                return amount/coins[0];
+            return 1e9;
         }
         if(dp[idx][amount]!=-1)
             return dp[idx][amount];
-        int notTake=0+helper(coins,amount,idx-1,dp);
-        int take=1e9;
-        
-        if(amount>=coins[idx])
-            take=1+helper(coins,amount-coins[idx],idx,dp);
-        
-        return dp[idx][amount]=min(notTake,take);
-        
+        int nt=0+helper(coins,amount,idx-1,dp);
+        int t=1e9;
+        if(coins[idx]<=amount)
+            t=1+helper(coins,amount-coins[idx],idx,dp);
+        return dp[idx][amount]=min(t,nt);
     }
     
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
-        // int ans=helper(coins,amount,n-1,dp);
-        // if(ans==1e9)
-        //     return -1;
-        // return ans;
-        
-        for(int i=1;i<=amount;i++)
-        {
-            if(i%coins[0]==0)
-                dp[0][i]=i/coins[0];
-            else
-                dp[0][i]=1e9;
-        }
-        
-        for(int idx=1;idx<n;idx++)
-        {
-            for(int tar=0;tar<=amount;tar++)
-            {
-                int notTake=0+dp[idx-1][tar];
-                int take=1e9;
-                if(tar>=coins[idx])
-                    take=1+dp[idx][tar-coins[idx]];
-                dp[idx][tar]=min(notTake,take);
-            }
-        }
-        if(dp[n-1][amount]==1e9)
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans= helper(coins,amount,n-1,dp);
+        if(ans>=1e9)
             return -1;
-        return dp[n-1][amount];
+        return ans;
     }
 };
