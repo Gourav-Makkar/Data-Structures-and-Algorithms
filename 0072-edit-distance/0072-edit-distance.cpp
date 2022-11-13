3c1ce20f11b709ba38ab5aa6b20b522755013c04
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-11-13 20:59:00 +0530

    Time: 23 ms (59.45%), Space: 9.1 MB (40.73%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -14,9 +14,9 @@ public:
         if(s1[i-1]==s2[j-1])
             return dp[i][j] = 0 + helper(s1,s2,i-1,j-1,dp);
         
-        int ins=1+helper(s1,s2,i,j-1,dp);
-        int del=1+helper(s1,s2,i-1,j,dp);
-        int rep=1+helper(s1,s2,i-1,j-1,dp);
+        int ins = 1 + helper(s1,s2,i,j-1,dp);
+        int del = 1 + helper(s1,s2,i-1,j,dp);
+        int rep = 1 + helper(s1,s2,i-1,j-1,dp);
             
         return dp[i][j] = min(ins,min(del,rep));
         
@@ -28,6 +28,30 @@ public:
         
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         
-        return helper(word1,word2,n,m,dp);
+        // return helper(word1,word2,n,m,dp);
+        for(int i=0;i<=n;i++)
+            dp[i][0]=i;
+        
+        for(int j=0;j<=m;j++)
+            dp[0][j]=j;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                    dp[i][j] = 0 + dp[i-1][j-1];
+        
+                else
+                {
+                    int ins = 1 + dp[i][j-1];
+                    int del = 1 + dp[i-1][j];
+                    int rep = 1 + dp[i-1][j-1];
+                        
+                    dp[i][j] = min(ins,min(del,rep));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
