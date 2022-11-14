c24ad55230d612b5f9cdf126c21b54e73a3550e1
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-11-14 15:58:16 +0530

    Time: 8 ms (79.57%), Space: 8.9 MB (30.51%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,24 +4,20 @@ public:
         vector<int>ans;
         stack<int>st;
         int n=nums2.size();
+        unordered_map<int,int>m;
         
-        vector<int>ng(n);
         st.push(nums2[n-1]);
-        ng[n-1]=-1;
+        m[nums2[n-1]]=-1;
         
         for(int i=n-2;i>=0;i--)
         {
             while(!st.empty() && nums2[i]>=st.top())
                 st.pop();
             int ele=(st.empty())?-1:st.top();
-            ng[i]=ele;
+            m[nums2[i]]=ele;
             st.push(nums2[i]);
         }
-        unordered_map<int,int>m;
-        
-        for(int i=0;i<n;i++)
-            m[nums2[i]]=ng[i];
-        
+             
         for(int i=0;i<nums1.size();i++)
         {
             ans.push_back(m[nums1[i]]);
