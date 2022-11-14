bb60630bf5ce8f81e0c87ab8b8391018ff9870ea
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-11-14 19:01:09 +0530

    Time: 81 ms (47.73%), Space: 23.9 MB (79.38%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -5,21 +5,17 @@ public:
        int n=nums.size();
        vector<int>ans(n);
         
-       nums.resize(2*n);
-       
-       for(int i=n;i<2*n;i++)
-           nums[i]=nums[i-n];
         
-       st.push(nums[2*n-1]);
+       st.push(nums[(2*n-1)%n]);
        ans[(2*n-1)%n]=-1;
        
        for(int i=2*n-2;i>=0;i--)
        {
-           while(!st.empty() && nums[i]>=st.top())
+           while(!st.empty() && nums[i%n]>=st.top())
                st.pop();
            int ele=(st.empty())?-1:st.top();
            ans[i%n]=ele;
-           st.push(nums[i]);
+           st.push(nums[i%n]);
        }
        return ans;
     }
