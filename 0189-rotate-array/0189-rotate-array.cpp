28b51a3946a0c5736e04074c8330c52a202c93b8
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-11-14 12:06:21 +0530

    Time: 47 ms (74.95%), Space: 25.1 MB (60.38%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -6,16 +6,10 @@ public:
             return;
         if(k>n)
             k=k%n;
-        vector<int>temp;
-        for(int i=n-k;i<n;i++)
-        {
-            temp.push_back(nums[i]);
-        }
         
-        for(int i=n-k-1;i>=0;i--)
-            nums[i+k]=nums[i];
+        reverse(nums.begin(),nums.begin()+n-k);
+        reverse(nums.begin()+n-k,nums.end());
         
-        for(int i=0;i<k;i++)
-            nums[i]=temp[i];  
+        reverse(nums.begin(),nums.end());
     }
 };
