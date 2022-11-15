69d8495dc509890ae51d1d234eb63daae72cbd2b
Gourav-Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-11-15 23:03:43 +0530

    Time: 35 ms (79.01%), Space: 25 MB (41.20%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,18 +2,31 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        vector<int>ps(n);
-        ps[0]=1;
-        for(int i=1;i<n;i++)
-            ps[i]=nums[i-1]*ps[i-1];
-        vector<int>ss(n);
-        ss[n-1]=1;
-        for(int i=n-2;i>=0;i--)
-            ss[i]=nums[i+1]*ss[i+1];
-        
-        vector<int>ans(n);
+        int p=1;
+        int ct=0;
         for(int i=0;i<n;i++)
-            ans[i]=ps[i]*ss[i];
+        {
+            if(nums[i]!=0)
+                p=p*nums[i];
+            else
+                ct++;
+        }
+        if(ct==n)
+            p=0;
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=0 && ct==0)
+                ans.push_back(p/nums[i]);
+            else if(nums[i]!=0 && ct>0)
+                ans.push_back(0);
+            else if(nums[i]==0 && ct>1)
+            {
+                ans.push_back(0);
+            }
+            else if(nums[i]==0 && ct==1)
+                ans.push_back(p);
+        }
         return ans;
     }
 };
