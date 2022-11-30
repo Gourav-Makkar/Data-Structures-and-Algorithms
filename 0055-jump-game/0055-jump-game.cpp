86ce207e9ea715fa3f1bdda6770508a88fe155f9
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-11-30 12:28:00 +0530

    Time: 1068 ms (17.36%), Space: 48.8 MB (24.63%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -20,7 +20,22 @@ public:
     
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-        return helper(nums,0,n-1,dp);
+        vector<bool>dp(n,false);
+        // return helper(nums,0,n-1,dp);
+        dp[n-1]=true;
+        for(int idx=n-2;idx>=0;idx--)
+        {
+            for(int i=1;i<=nums[idx];i++)
+            {
+                if(idx+i>=n)
+                    break;
+                if(dp[idx+i])
+                {
+                    dp[idx]=true;
+                    break;
+                }
+            }
+        }
+       return dp[0];
     }
 };
