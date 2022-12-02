848840eb1166e4762cceb96bb75a9c64a3faad85
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-12-02 19:32:25 +0530

    Time: 33 ms (62.20%), Space: 12.7 MB (70.98%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -5,10 +5,8 @@ public:
         int m=str2.length();
         
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        
         for(int i=0;i<=n;i++)
             dp[i][0]=0;
-        
         for(int i=0;i<=m;i++)
             dp[0][i]=0;
         
@@ -22,37 +20,36 @@ public:
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        int l=dp[n][m];
-        string ans="";
         
+        string ans;
         int i=n,j=m;
-        while(i>=1 && j>=1)
+        while(i>0 &&j>0)
         {
             if(str1[i-1]==str2[j-1])
             {
-                ans+=str1[i-1];
+                ans.push_back(str1[i-1]);
                 i--;
                 j--;
             }
             else if(dp[i-1][j]>=dp[i][j-1])
             {
-                ans+=str1[i-1];
+                ans.push_back(str1[i-1]);
                 i--;
             }
             else
             {
-                ans+=str2[j-1];
+                ans.push_back(str2[j-1]);
                 j--;
             }
         }
         while(i>0)
         {
-            ans+=str1[i-1];
+            ans.push_back(str1[i-1]);
             i--;
         }
         while(j>0)
         {
-            ans+=str2[j-1];
+            ans.push_back(str2[j-1]);
             j--;
         }
         
