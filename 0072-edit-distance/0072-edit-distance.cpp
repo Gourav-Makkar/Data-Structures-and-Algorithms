410a158431279f4a316002363e3f4ef7f5ef16d7
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-12-03 00:47:00 +0530

    Time: 23 ms (59.46%), Space: 9.1 MB (41.63%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -27,7 +27,30 @@ public:
         
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         
-        return helper(word1,word2,n,m,dp);
+        // return helper(word1,word2,n,m,dp);
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=i;
+        }
+        for(int i=0;i<=m;i++)
+            dp[0][i]=i;
         
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                    dp[i][j]=0+dp[i-1][j-1];
+                else
+                {
+                    int ins=1 + dp[i][j-1];
+                    int del=1 + dp[i-1][j];
+                    int rep=1+ dp[i-1][j-1];
+                    
+                    dp[i][j]=min(ins,min(del,rep));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
