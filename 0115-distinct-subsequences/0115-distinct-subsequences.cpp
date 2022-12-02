2574df4ed294c69ddb288aa102b60603bb298c46
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-12-02 20:00:03 +0530

    Time: 143 ms (6.76%), Space: 18.5 MB (47.44%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int mod=1e9+7;
-    int helper(string s,string t,int i,int j,vector<vector<int>>&dp)
+    int helper(string s,string t,int idx1,int idx2,vector<vector<int>>&dp)
     {
-        if(j==0)
+        if(idx2==0)
             return 1;
-        if(i==0)
+        if(idx1==0)
             return 0;
+        if(dp[idx1][idx2]!=-1)
+            return dp[idx1][idx2];
+        int ct=0;
+        if(s[idx1-1]==t[idx2-1])
+            ct+=helper(s,t,idx1-1,idx2-1,dp);
+        ct+=helper(s,t,idx1-1,idx2,dp);
         
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int take=0,notTake=0,notMatch=0;
-        if(s[i-1]==t[j-1])
-        {
-            take=helper(s,t,i-1,j-1,dp);
-            notTake=helper(s,t,i-1,j,dp);
-        }
-        else
-            notMatch=helper(s,t,i-1,j,dp);
-        return dp[i][j]=take+notTake+notMatch;
+        return dp[idx1][idx2]=ct;
     }
     
     int numDistinct(string s, string t) {
@@ -27,29 +23,23 @@ public:
         
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         // return helper(s,t,n,m,dp);
-        
-        for(int i=0;i<=m;i++)
-            dp[0][i]=0;
-        
         for(int i=0;i<=n;i++)
             dp[i][0]=1;
+        for(int i=1;i<=m;i++)
+            dp[0][i]=0;
         
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
-                int take=0,notTake=0,notMatch=0;
+                int ct=0;
                 if(s[i-1]==t[j-1])
-                {
-                    take=dp[i-1][j-1];
-                    notTake=dp[i-1][j];
-                }
-                else
-                    notMatch=dp[i-1][j];
-                dp[i][j]=(take+notTake+notMatch)%mod;
-            } 
+                    ct+=(dp[i-1][j-1])%mod;
+                ct+=(dp[i-1][j])%mod;
+                
+                dp[i][j]=ct;
+            }
         }
         return dp[n][m];
-        
     }
 };
