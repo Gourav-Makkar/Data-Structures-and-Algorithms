7493bdc7506ddb610895d0de5485e77d5fa0f1f8
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-12-14 11:56:19 +0530

    Time: 5 ms (26.88%), Space: 7.8 MB (85.96%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
     
-    int helper(vector<int>arr)
+    int helper(vector<int>&nums)
     {
-        int prev=arr[0];
-        int prev2=0;
-        for(int i=1;i<arr.size();i++)
+        int prev2=0,prev=nums[0];
+        
+        for(int i=1;i<nums.size();i++)
         {
-            int pick=arr[i];
+            int take=nums[i];
             if(i-2>=0)
-                pick+=prev2;
-            int nonPick=0+prev;
+                take+=prev2;
+            int notTake=0+prev;
             
-            int curr=max(pick,nonPick);
             prev2=prev;
-            prev=curr;
+            prev=max(take,notTake);
         }
         return prev;
     }
     
     int rob(vector<int>& nums) {
-        vector<int>temp1,temp2;
+        int n=nums.size();
         if(nums.size()==1)
             return nums[0];
-        for(int i=0;i<nums.size();i++)
+        
+        vector<int>temp1,temp2;
+        
+        for(int i=0;i<n;i++)
         {
-            if(i!=nums.size()-1)
-                temp1.push_back(nums[i]);
             if(i!=0)
+                temp1.push_back(nums[i]);
+            if(i!=n-1)
                 temp2.push_back(nums[i]);
-                
         }
+        
         int ans1=helper(temp1);
         int ans2=helper(temp2);
         
