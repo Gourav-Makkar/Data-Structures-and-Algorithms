b0024b6c64535c743cfcba325ae04e6a876ab034
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2022-12-14 11:44:40 +0530

    Time: 2 ms (60.85%), Space: 7.7 MB (78.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
+    
+    int helper(vector<int>&nums,int idx,vector<int>&dp)
+    {
+        if(idx<0)
+            return 0;
+        
+        if(dp[idx]!=-1)
+            return dp[idx];
+        
+        int take=nums[idx]+helper(nums,idx-2,dp);
+        int notTake=0+helper(nums,idx-1,dp);
+        
+        return dp[idx]=max(take,notTake);
+    }
+    
     int rob(vector<int>& nums) {
-        // Your code here
-        // int n=nums.size();
-        // vector<int>dp(n,-1);
-        // dp[0]=nums[0];
-        
-        // for(int i=1;i<n;i++)
-        // {
-        //     int pick=nums[i];
-        //     if(i-2>=0)
-        //       pick+=dp[i-2];
-        //     int nonPick=0+dp[i-1];
-            
-        //     dp[i]=max(pick,nonPick);
-        // }
-        // return dp[n-1];
-        
         int n=nums.size();
-        int prev=nums[0];
-        int prev2=0;
-        
-        for(int i=1;i<n;i++)
-        {
-            int pick=nums[i];
-            if(i-2>=0)
-              pick+=prev2;
-            int nonPick=0+prev;
-            
-            int curr=max(pick,nonPick);
-            prev2=prev;
-            prev=curr;
-            
-        }
-        return prev;
+        vector<int>dp(n,-1);
+        return helper(nums,n-1,dp);
     }
 };
