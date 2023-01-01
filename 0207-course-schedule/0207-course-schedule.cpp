638f3efdaa654090059e8aec8c024878ae3c19ef
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-01-01 22:36:22 +0530

    Time: 25 ms (83.05%), Space: 13.9 MB (52.47%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<int>adj[n],ans;
+        
+        for(auto it:pre)
+            adj[it[1]].push_back(it[0]);
+        
+        vector<int>indeg(n,0);
+        
+        for(int i=0;i<n;i++)
+        {
+            for(auto it:adj[i])
+                indeg[it]++;
+        }
+        queue<int>q;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(indeg[i]==0)
+                q.push(i);
+        }
+        
+        while(!q.empty())
+        {
+            int curr=q.front();
+            q.pop();
+            ans.push_back(curr);
+            for(auto it:adj[curr])
+            {
+                indeg[it]--;
+                if(indeg[it]==0)
+                    q.push(it);
+            }
+        }
+        
+        return ans.size()==n;
+    }
+};
