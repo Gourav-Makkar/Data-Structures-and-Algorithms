470cada71f241e4b25c47d983c0f26a46c4e3d4c
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-01-10 19:49:11 +0530

    Time: 527 ms (14.86%), Space: 164.2 MB (14.01%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     
-    void helper(int n,int idx,int k,vector<int>curr,vector<vector<int>>&ans)
+    void helper(int idx,int n,int k,vector<int>curr,vector<vector<int>>&ans)
     {
         if(curr.size()==k)
         {
             ans.push_back(curr);
             return;
         }
+        
         for(int i=idx;i<=n;i++)
         {
             curr.push_back(i);
-            helper(n,i+1,k,curr,ans);
+            helper(i+1,n,k,curr,ans);
             curr.pop_back();
         }
     }
     
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>>ans;
-        vector<int>curr;
-        helper(n,1,k,curr,ans);
         
+        helper(1,n,k,{},ans);
         return ans;
     }
 };
