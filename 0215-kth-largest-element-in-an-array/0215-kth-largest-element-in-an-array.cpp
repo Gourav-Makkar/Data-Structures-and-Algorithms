806d7938636736324be6282cf1eeff56ceddd9d9
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-01-11 12:56:40 +0530

    Time: 89 ms (97.26%), Space: 45.5 MB (88.72%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,15 +1,56 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        int n=nums.size();
-        priority_queue<int,vector<int>,greater<int>>p;
-        
-        for(int i=0;i<n;i++)
+    
+    
+    int check(vector<int>&nums,int mid)
+    {
+        int ct=0;
+        for(auto it:nums)
         {
-            p.push(nums[i]);
-            if(p.size()>k)
-                p.pop();
+            if(it>=mid)
+                ct++;
         }
-        return p.top();
+        return ct;
+    }
+    
+    bool find(vector<int>&nums,int mid)
+    {
+        for(auto it:nums)
+        {
+            if(it==mid)
+                return true;
+        }
+        return false;
+    }
+    
+    int findKthLargest(vector<int>& nums, int k) {
+        int st=INT_MAX,en=INT_MIN;
+//         unordered_set<int>s(nums.begin(),nums.end());
+        
+//         nums.resize(0);
+//         for(auto it:s)
+//             nums.push_back(it);
+        
+        for(auto it:nums)
+        {
+            st=min(st,it);
+            en=max(en,it);
+        }
+        
+        int ans=-1;
+        
+        while(st<=en)
+        {
+            int mid=st+(en-st)/2;
+        
+            if(check(nums,mid)>=k)
+            {
+                ans=mid;
+                st=mid+1;
+            }
+            else
+                en=mid-1;
+        }
+        return ans;
     }
 };
