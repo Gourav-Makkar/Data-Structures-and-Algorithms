4642b08b6368ec292342ee84f43f8778c56b4855
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-01-19 10:48:31 +0530

    Time: 12 ms (92.33%), Space: 19.7 MB (85.21%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -9,19 +9,16 @@ public:
         
         while(l<=r)
         {
+            lmax=max(lmax,height[l]);
+            rmax=max(rmax,height[r]);
+            
             if(height[l]<=height[r])
-            {
-                lmax=max(lmax,height[l]);
-                rmax=max(rmax,height[r]);
-                
+            {   
                 ans+=min(lmax,rmax)-height[l];
                 l++;
             }
             else
             {
-                lmax=max(lmax,height[l]);
-                rmax=max(rmax,height[r]);
-                
                 ans+=min(lmax,rmax)-height[r];
                 r--;
             }
