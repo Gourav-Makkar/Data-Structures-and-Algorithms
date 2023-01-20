299e09e4738f797b26a63b8833bc8aa54a0cb9fe
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-01-20 13:49:01 +0530

    Time: 15 ms (36.37%), Space: 11.2 MB (64.61%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,57 +17,43 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node* h=NULL,*t=NULL;
-        unordered_map<Node*,int>m1;
-        unordered_map<int,Node*>m2;
+       Node* curr=head;
         
-        int idx=0;
-        Node* curr=head;
-        while(curr!=NULL)
-        {
-            m1[curr]=idx;
-            int v=curr->val;
-            
-            Node* temp=new Node(v);
-            m2[idx]=temp;
-            
-            // if(h==NULL)
-            // {
-            //     h=temp;
-            //     t=temp;
-            // }
-            
-            idx++;
-            curr=curr->next;
-        }
-        curr=head;
+       while(curr!=NULL)
+       {
+           int v=curr->val;
+           Node* temp=new Node(v);
+           temp->next=curr->next;
+           curr->next=temp;
+           curr=temp->next;
+       }
         
-        while(curr!=NULL)
-        {
-            int idx=m1[curr];
-            
-            Node* temp=m2[idx];
-            if(h==NULL)
-            {
-                h=temp;
-                t=temp;
-            }
-            
-            else
-            {
-                t->next=temp;
-                t=t->next;
-            }
-            if(curr->random==NULL)
-                temp->random=NULL;
-            else
-            {
-                int ridx=m1[curr->random];
-                temp->random=m2[ridx];
-            }
-            
-            curr=curr->next;
-        }
+       curr=head;
+       while(curr!=NULL)
+       {
+           Node* temp=curr->next;
+           if(curr->random!=NULL)
+               temp->random=curr->random->next;
+           curr=temp->next;
+       }
+       Node* h=NULL,*t=NULL;
+       curr=head;
+       while(curr!=NULL)
+       {
+           if(h==NULL)
+           {
+               h=curr->next;
+               t=curr->next;
+           }
+           else
+           {
+               t->next=curr->next;
+               t=t->next;
+           }
+               
+           curr->next=curr->next->next;
+           curr=curr->next;
+       }
         return h;
     }
 };
