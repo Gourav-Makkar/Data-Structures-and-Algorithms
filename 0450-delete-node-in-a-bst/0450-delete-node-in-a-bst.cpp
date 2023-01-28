b5e51972045c43d3ab49109d62caf1d8b8b48b6d
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-01-28 22:59:12 +0530

    Time: 47 ms (40.20%), Space: 32.7 MB (48.41%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -14,22 +14,26 @@ public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(root==NULL)
             return NULL;
-        if(root->val==key)
+        
+        if(root->val<key)
+            root->right=deleteNode(root->right,key);
+        
+        else if(root->val>key)
+            root->left=deleteNode(root->left,key);
+        
+        else
         {
             if(root->right==NULL)
-                return root->left;
+              return root->left;
+            
             TreeNode* curr=root->right;
+            
             while(curr->left!=NULL)
                 curr=curr->left;
             
             curr->left=root->left;
-            return root->right;   
+            return root->right;
         }
-        else if(root->val<key)
-          root->right=deleteNode(root->right,key);
-        else
-          root->left=deleteNode(root->left,key);
-        
         return root;
     }
 };
