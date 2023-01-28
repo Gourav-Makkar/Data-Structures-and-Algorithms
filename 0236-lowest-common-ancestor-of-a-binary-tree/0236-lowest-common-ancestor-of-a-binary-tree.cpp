b51f2db691d937ee650ca31cd4d823c413dcfe9b
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-01-28 13:18:02 +0530

    Time: 27 ms (33.21%), Space: 17.5 MB (6.73%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,44 +10,39 @@
 class Solution {
 public:
     
-    void helper(TreeNode* root,TreeNode* des,vector<TreeNode*>&path)
+    void helper(TreeNode* root,TreeNode* p,vector<TreeNode*>&path)
     {
         if(root==NULL)
             return;
-        if(root==des)
+        if(root==p)
         {
             path.push_back(root);
             return;
         }
         path.push_back(root);
-        helper(root->left,des,path);
-        helper(root->right,des,path);
-        if(path.back()==root)
+        
+        helper(root->left,p,path);
+        helper(root->right,p,path);
+        
+        if(path.back()!=p)
             path.pop_back();
     }
     
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL)
-            return NULL;
-        
         vector<TreeNode*>path1,path2;
-        
-        for(auto it:path1)
-            cout<<it->val<<" ";
-        cout<<endl;
-        for(auto it:path2)
-            cout<<it->val<<" ";
         
         helper(root,p,path1);
         helper(root,q,path2);
         
-        int i=0,j=0;
+        int i=0,j=0,n1=path1.size(),n2=path2.size();
         TreeNode* ans;
         
-        while(i<path1.size() && j<path2.size())
+        while(i<n1 && j<n2)
         {
             if(path1[i]==path2[j])
+            {
                 ans=path1[i];
+            }
             i++;
             j++;
         }
