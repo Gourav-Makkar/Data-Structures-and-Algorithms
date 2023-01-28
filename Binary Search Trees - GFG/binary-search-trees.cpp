79201149a295b9ba8fc7d4c6c953627cdd3efea6
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-01-28 20:10:58 +0530

    Added solution - LeetHub

---
+++ Binary Search Trees - GFG/binary-search-trees.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+// {Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    bool isBSTTraversal(vector<int>& nums) {
+         int mx=INT_MIN;
+         
+         for(int i=0;i<nums.size();i++)
+         {
+             if(nums[i]>mx)
+             {
+                 mx=nums[i];
+             }
+             else
+               return false;
+         }
+         return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++) {
+            int k;
+            cin >> k;
+            nums[i] = k;
+        }
+
+        Solution ob;
+        bool res = ob.isBSTTraversal(nums);
+        if (res)
+            cout << "True";
+        else
+            cout << "False";
+
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
