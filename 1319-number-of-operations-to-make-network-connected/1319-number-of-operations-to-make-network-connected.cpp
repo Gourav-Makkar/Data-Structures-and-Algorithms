ae518a685d4c29d66e2ef26f7fc2b89fc9153cd1
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-01-31 10:40:06 +0530

    Time: 143 ms (65.07%), Space: 55.2 MB (25.25%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,86 +1,40 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-class DisjointSet{
-    vector<int>parent,rank,size;
-public:
-    DisjointSet(int n)
-    {
-        parent.resize(n+1);
-        rank.resize(n+1,0);
-        size.resize(n+1,1);
-
-        for(int i=0;i<=n;i++)
-          parent[i]=i;
-    }
-    int findUPar(int node)
-    {
-        if(parent[node]==node)
-          return node;
-        return parent[node]=findUPar(parent[node]);
-    }
-
-    void UnionByRank(int u,int v)
-    {
-        int pu=findUPar(u);
-        int pv=findUPar(v);
-
-        if(pu==pv)
-          return;
-        
-        if(rank[pu]<rank[pv])
-          parent[pu]=pv;
-        else if(rank[pv]<rank[pu])
-          parent[pv]=pu;
-        else{
-            parent[pv]=pu;
-            rank[pu]++;
-        }
-
-    }
-    void UnionBySize(int u,int v)
-    {
-        int pu=findUPar(u);
-        int pv=findUPar(v);
-
-        if(pu==pv)
-          return;
-        
-        if(size[pu]<size[pv])
-        {
-            parent[pu]=pv;
-            size[pv]+=size[pu];
-        }
-        else{
-            parent[pv]=pu;
-            size[pu]+=size[pv];
-        }
-
-    }
-};
-
 class Solution {
 public:
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        DisjointSet ds(n);
-        int tc=0,cc=0;
+    
+    void dfs(int curr,vector<int>adj[],vector<int>&vis)
+    {
+        vis[curr]=1;
         
-        for(auto it:connections)
+        for(auto it:adj[curr])
         {
-            if(ds.findUPar(it[0])==ds.findUPar(it[1]))
-                tc++;
-            else
-                ds.UnionBySize(it[0],it[1]);
+            if(vis[it]==0)
+                dfs(it,adj,vis);
         }
+    }
+    
+    int makeConnected(int n, vector<vector<int>>& con) {
+        vector<int>adj[n];
+        
+        if(con.size()<n-1)
+            return -1;
+        
+        for(auto it:con)
+        {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int>vis(n,0);
+        int c=0;
         
         for(int i=0;i<n;i++)
         {
-           if(ds.findUPar(i)==i)
-               cc++;
-              
+            if(vis[i]==0)
+            {
+                c++;
+                dfs(i,adj,vis);
+            }
         }
-        if(cc-1>tc)
-            return -1;
-        return cc-1; 
+       
+        return c-1;
     }
 };
