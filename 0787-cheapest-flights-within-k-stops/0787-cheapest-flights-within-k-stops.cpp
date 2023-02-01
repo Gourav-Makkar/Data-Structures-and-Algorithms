6252b97dec27ba9a864484937687f73982d1c11e
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-02-01 15:52:12 +0530

    Time: 28 ms (72.18%), Space: 13.6 MB (56.80%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int>cost(n,INT_MAX);
-        cost[src]=0;
+        vector<pair<int,int>>adj[n];
         
-        vector<vector<int>>adj[n];
         for(auto it:flights)
+        {
             adj[it[0]].push_back({it[1],it[2]});
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
-        pq.push({0,src,0});
-        
+        }
+        vector<int>price(n,INT_MAX);
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>>pq;
+        pq.push({0,{0,src}});
+        price[src]=0;
         while(!pq.empty())
         {
-            vector<int>p=pq.top();
+            int stops=pq.top().first;
+            int cost=pq.top().second.first;
+            int curr=pq.top().second.second;
+
             pq.pop();
             
-            int stops=p[0];
-            int node=p[1];
-            int cc=p[2];
-            
             if(stops>k)
+                break;
+            if(curr==dst)
                 continue;
-            
-            for(auto it:adj[node])
+            for(auto it:adj[curr])
             {
-                if(cc+it[1]<cost[it[0]])
+                if(cost+it.second<price[it.first])
                 {
-                    cost[it[0]]=cc+it[1];
-                    pq.push({stops+1,it[0],cost[it[0]]});
+                    price[it.first]=cost+it.second;
+                    pq.push({stops+1,{price[it.first],it.first}});
                 }
             }
         }
-        if(cost[dst]==INT_MAX)
+        if(price[dst]==INT_MAX)
             return -1;
-        return cost[dst];
+        return price[dst];
     }
 };
