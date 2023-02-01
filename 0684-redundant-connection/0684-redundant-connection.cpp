2ce8e769006e983661095d604df38c0f3b75861b
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-02-01 16:52:19 +0530

    Time: 4 ms (94.01%), Space: 9.1 MB (48.73%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,81 +1,32 @@
-
-class DisjointSet{
-    vector<int>parent,rank,size;
-public:
-    DisjointSet(int n)
-    {
-        parent.resize(n+1);
-        rank.resize(n+1,0);
-        size.resize(n+1,1);
-
-        for(int i=0;i<=n;i++)
-          parent[i]=i;
-    }
-    int findUPar(int node)
-    {
-        if(parent[node]==node)
-          return node;
-        return parent[node]=findUPar(parent[node]);
-    }
-
-    void UnionByRank(int u,int v)
-    {
-        int pu=findUPar(u);
-        int pv=findUPar(v);
-
-        if(pu==pv)
-          return;
-        
-        if(rank[pu]<rank[pv])
-          parent[pu]=pv;
-        else if(rank[pv]<rank[pu])
-          parent[pv]=pu;
-        else{
-            parent[pv]=pu;
-            rank[pu]++;
-        }
-
-    }
-    void UnionBySize(int u,int v)
-    {
-        int pu=findUPar(u);
-        int pv=findUPar(v);
-
-        if(pu==pv)
-          return;
-        
-        if(size[pu]<size[pv])
-        {
-            parent[pu]=pv;
-            size[pv]+=size[pu];
-        }
-        else{
-            parent[pv]=pu;
-            size[pu]+=size[pv];
-        }
-
-    }
-};
-
 class Solution {
 public:
+    
+    int findPar(int x,vector<int>&par)
+    {
+        if(par[x]==x)
+            return x;
+        return par[x]=findPar(par[x],par);
+    }
+    
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int st,en;
-        
         int n=edges.size();
-        DisjointSet ds(n);
+        vector<int>par(n+1);
+        for(int i=1;i<=n;i++)
+            par[i]=i;
         
         for(auto it:edges)
         {
-            if(ds.findUPar(it[0]) == ds.findUPar(it[1]))
-            {
-                st=it[0];
-                en=it[1];
-            }
-            else
-                ds.UnionBySize(it[0],it[1]);
+            int u=it[0];
+            int v=it[1];
+            
+            int pu=findPar(u,par);
+            int pv=findPar(v,par);
+            
+            if(pu==pv)
+                return {u,v};
+            
+            par[pu]=par[pv];
         }
-        
-        return {st,en};
+        return {-1,-1};
     }
 };
