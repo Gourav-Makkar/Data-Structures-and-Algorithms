5aebfa5bd2fae5082a59646b4b357f197f6d06ab
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-02-01 14:07:44 +0530

    Time: 176 ms (64.34%), Space: 20.1 MB (61.45%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,66 +1,59 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& grid) {
-        int r=grid.size();
-        int c=grid[0].size();
-        
-        vector<vector<int>>ef(r,vector<int>(c,INT_MAX));
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int r=heights.size();
+        int c=heights[0].size();
+        // int ans=INT_MAX;
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        
         pq.push({0,{0,0}});
-        ef[0][0]=0;
+        
+        vector<vector<int>>ans(r,vector<int>(c,INT_MAX));
+        ans[0][0]=0;
         
         while(!pq.empty())
         {
-            pair<int,pair<int,int>>p=pq.top();
+            int eff=pq.top().first;
+            int cr=pq.top().second.first;
+            int cc=pq.top().second.second;
             pq.pop();
-            
-            int cr=p.second.first;
-            int cc=p.second.second;
-            int e=p.first;
             
             if(cr>0)
             {
-                int ce=abs(grid[cr][cc]-grid[cr-1][cc]);
-                ce=max(ce,e);
-                if(ce<ef[cr-1][cc])
+                int cf=max(eff,abs(heights[cr][cc]-heights[cr-1][cc]));
+                if(cf<ans[cr-1][cc])
                 {
-                    ef[cr-1][cc]=ce;
-                    pq.push({ce,{cr-1,cc}});
+                    pq.push({cf,{cr-1,cc}});
+                    ans[cr-1][cc]=cf;
                 }
             }
             if(cc<c-1)
             {
-                int ce=abs(grid[cr][cc]-grid[cr][cc+1]);
-                ce=max(ce,e);
-                if(ce<ef[cr][cc+1])
+                int cf=max(eff,abs(heights[cr][cc]-heights[cr][cc+1]));
+                if(cf<ans[cr][cc+1])
                 {
-                    ef[cr][cc+1]=ce;
-                    pq.push({ce,{cr,cc+1}});
+                    pq.push({cf,{cr,cc+1}});
+                    ans[cr][cc+1]=cf;
                 }
             }
             if(cr<r-1)
             {
-                int ce=abs(grid[cr][cc]-grid[cr+1][cc]);
-                ce=max(ce,e);
-                if(ce<ef[cr+1][cc])
+                int cf=max(eff,abs(heights[cr][cc]-heights[cr+1][cc]));
+                if(cf<ans[cr+1][cc])
                 {
-                    ef[cr+1][cc]=ce;
-                    pq.push({ce,{cr+1,cc}});
+                    pq.push({cf,{cr+1,cc}});
+                    ans[cr+1][cc]=cf;
                 }
             }
             if(cc>0)
             {
-                int ce=abs(grid[cr][cc]-grid[cr][cc-1]);
-                ce=max(ce,e);
-                if(ce<ef[cr][cc-1])
+                int cf=max(eff,abs(heights[cr][cc]-heights[cr][cc-1]));
+                if(cf<ans[cr][cc-1])
                 {
-                    ef[cr][cc-1]=ce;
-                    pq.push({ce,{cr,cc-1}});
+                    pq.push({cf,{cr,cc-1}});
+                    ans[cr][cc-1]=cf;
                 }
             }
         }
-        
-        return ef[r-1][c-1];
+        return ans[r-1][c-1];
     }
 };
