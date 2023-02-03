c07ba1a1a61ef2db3a083b36cbe00bb2cb25b085
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-02-04 00:07:23 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
@@ -1,72 +1,36 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function template for C++
 class Solution{
 public:	
+
+    int helper(int idx,int *arr,int n,vector<int>&dp)
+    {
+        if(idx>=n)
+          return 0;
+        
+        if(dp[idx]!=-1)
+          return dp[idx];
+        
+        int t=arr[idx]+helper(idx+2,arr,n,dp);
+        int nt=helper(idx+1,arr,n,dp);
+        
+        return dp[idx]=max(t,nt);
+    }
+
 	// calculate the maximum sum with out adjacent
-	
-// 	int helper(vector<int>arr,int idx,vector<int>&dp)
-// 	{
-// 	    if(idx==0)
-// 	      return arr[0];
-// 	    if(idx<0)
-// 	      return 0;
-// 	    if(dp[idx]!=-1)
-// 	      return dp[idx];
-// 	    int pick=arr[idx]+helper(arr,idx-2,dp);
-// 	    int nonPick=0+helper(arr,idx-1,dp);
-	    
-// 	    return dp[idx]=max(pick,nonPick);
-// 	}
-	
 	int findMaxSum(int *arr, int n) {
-	    
-	   int prev=arr[0];
-	   int prev2=0;
-	   for(int i=1;i<n;i++)
-	   {
-	       int pick=arr[i];
-	       if(i>=2)
-	         pick+=prev2;
-	       int nonPick=0+prev;
-	       
-	       int curr=max(pick,nonPick);
-	       prev2=prev;
-	       prev=curr;
-	   }
-	   return prev;
-	    // code here
-	   // vector<int>v(n);
-	   // vector<int>dp(n,0);
-	   // for(int i=0;i<n;i++)
-	   // {
-	   //     v[i]=arr[i];
-	   // }
-	   
-	   //dp[0]=arr[0];
-	   //for(int i=1;i<n;i++)
-	   //{
-	       
-	   //    int pick=arr[i];
-	   //    if(i>=2)
-	   //      pick+=dp[i-2];
-	   //    int nonPick=0+dp[i-1];
-	       
-	   //    dp[i]=max(pick,nonPick);
-	   //}
-	   
-	   //return dp[n-1];
-	    
-	    
+	   vector<int>dp(n,-1); 
+	   return helper(0,arr,n,dp);
 	}
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main() {
     int t;
@@ -83,4 +47,5 @@ int main() {
         cout << ans << "\n";
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
