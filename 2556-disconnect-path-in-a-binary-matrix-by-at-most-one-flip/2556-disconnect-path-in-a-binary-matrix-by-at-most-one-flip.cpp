0c69c865c80747e76bcb5307bd2945d802c4bfd2
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-02-04 23:36:11 +0530

    Time: 123 ms (45.45%), Space: 38.8 MB (9.09%) - LeetHub

--- 2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip.cpp
+++ 2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
        
-     bool path(vector<vector<int>>& grid, int i, int j, vector<pair<int, int>> &v) {
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size()) return false;
-        if(grid[i][j] == 0) return false;
-        if(i == grid.size()-1 && j == grid[0].size()-1) {
+    bool helper(int cr,int cc,vector<vector<int>>&grid,int r,int c,vector<pair<int,int>>&path)
+    {
+        if(cr==r-1 && cc==c-1)
             return true;
-        }
-        if(!(i==0 && j==0)) grid[i][j] = 0;
-        v.push_back({i, j});
-        if(path(grid, i, j+1, v)) {
+        
+        if(cr<0 || cr==r || cc<0 || cc==c || grid[cr][cc]==0)
+            return false;
+        
+        if(!(cr==0 && cc==0)) grid[cr][cc] = 0;
+        path.push_back({cr,cc});
+        
+        bool down=helper(cr+1,cc,grid,r,c,path);
+        if(down)
             return true;
-        }
-        if(path(grid, i+1, j, v)) {
+        
+        bool right=helper(cr,cc+1,grid,r,c,path);
+        if(right)
             return true;
-        }
-        v.pop_back();
+        
+        path.pop_back();
         return false;
     }
     
     bool isPossibleToCutPath(vector<vector<int>>& grid) {
-        int c1 = 0;
-        int c2 = 0;
-        if(grid.size() <= 1 && grid[0].size() <=2) return false;
-        vector<pair<int, int>> v;
-        if(path(grid, 0, 0, v)) {
-            for(auto [x, y]: v) {
-                if(x==0 && y == 0) continue;
-                grid[x][y] = 0;
-            }
-        }
-        v.clear();
-        return path(grid, 0, 0, v) == false;
+        int r=grid.size();
+        int c=grid[0].size();
+        
+        vector<pair<int,int>>path;
+        helper(0,0,grid,r,c,path);
+        
+        if(helper(0,0,grid,r,c,path))
+            return false;
+        return true;
     }
 };
