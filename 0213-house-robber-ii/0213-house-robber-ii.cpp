ff4841e8e5128610fe3157f159905f63a6cc5cb8
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-02-06 11:39:32 +0530

    Time: 3 ms (46.22%), Space: 8.1 MB (21.66%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
-    
-    int helper(vector<int>&nums,int idx,vector<int>&dp)
-    {
-       if(idx<0)
-           return 0;
-        
-       if(dp[idx]!=-1)
-           return dp[idx];
-        
-       int take=nums[idx]+helper(nums,idx-2,dp);
-       int notTake=0+helper(nums,idx-1,dp);
-        
-       return dp[idx]=max(take,notTake);
-    }
-    
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if(nums.size()==1)
+        if(n==1)
             return nums[0];
+        if(n==2)
+            return max(nums[0],nums[1]);
+        vector<int>dp1(n-1);
+        dp1[0]=nums[0];
+        dp1[1]=max(dp1[0],nums[1]);
         
-        vector<int>temp1,temp2;
+        for(int i=2;i<n-1;i++)
+            dp1[i]=max(dp1[i-1],dp1[i-2]+nums[i]);
         
-        for(int i=0;i<n;i++)
+        vector<int>dp2(n-1);
+        dp2[0]=nums[1];
+        dp2[1]=max(dp2[0],nums[2]);
+        
+        for(int i=3;i<n;i++)
         {
-            if(i!=0)
-                temp1.push_back(nums[i]);
-            if(i!=n-1)
-                temp2.push_back(nums[i]);
+            dp2[i-1]=max(dp2[i-2],dp2[i-3]+nums[i]);
         }
-        
-        vector<int>dp1(n-1,-1);
-        int ans1=helper(temp1,temp1.size()-1,dp1);
-        
-        vector<int>dp2(n-1,-1);
-        
-        int ans2=helper(temp2,temp2.size()-1,dp2);
-        
-        return max(ans1,ans2);
+        return max(dp1[n-2],dp2[n-2]);
     }
 };
