290fbfbf6f537bba8ee8fa66099b07b216807f92
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-02-07 23:40:36 +0530

    Time: 728 ms (13.94%), Space: 557.6 MB (8.70%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
     
-    int helper(vector<vector<int>>&grid,int i1,int i2,int j1,int j2,int n,vector<vector<vector<vector<int>>>>&dp)
+    int helper(int r1,int c1,int r2,int c2,vector<vector<int>>&grid,int n,vector<vector<vector<vector<int>>>>&dp)
     {
-        if(i1>=n || i2>=n || j1>=n || j2>=n)
-            return -1e8;
-        if(grid[i1][j1]==-1 || grid[i2][j2]==-1)
-            return -1e8;
-        if(i1==n-1 && j1==n-1)
-            return grid[i1][j1];
-        if(i2==n-1 && j2==n-1)
-            return grid[i2][j2];
+        if(r1==n || c1==n || r2==n || c2==n || grid[r1][c1]==-1 || grid[r2][c2]==-1)
+            return -1e9;
         
-        if(dp[i1][i2][j1][j2]!=-1)
-            return dp[i1][i2][j1][j2];
-        int cherry=0;
-        if(i1==i2 && j1==j2)
-            cherry=grid[i1][j1];
+        if(r1==n-1 && c1==n-1)
+            return grid[r1][c1];
+        if(r2==n-1 && c2==n-1)
+            return grid[r2][c2];
+        
+        if(dp[r1][c1][r2][c2]!=-1)
+            return dp[r1][c1][r2][c2];
+        
+        int ans=0;
+        
+        if(r1==r2 && c1==c2)
+            ans=grid[r1][c1];
         else
-            cherry=grid[i1][j1]+grid[i2][j2];
+            ans=grid[r1][c1]+grid[r2][c2];
         
-        int dd=helper(grid,i1+1,i2+1,j1,j2,n,dp);
-        int dr=helper(grid,i1+1,i2,j1,j2+1,n,dp);
-        int rd=helper(grid,i1,i2+1,j1+1,j2,n,dp);
-        int rr=helper(grid,i1,i2,j1+1,j2+1,n,dp);
-        
-        int mx=max(dd,max(dr,max(rd,rr)));
-        cherry+=mx;
-        
-        return dp[i1][i2][j1][j2]=cherry;
-            
+        int dd=helper(r1+1,c1,r2+1,c2,grid,n,dp);
+        int dr=helper(r1+1,c1,r2,c2+1,grid,n,dp);
+        int rd=helper(r1,c1+1,r2+1,c2,grid,n,dp);
+        int rr=helper(r1,c1+1,r2,c2+1,grid,n,dp);
+    
+        return dp[r1][c1][r2][c2]=max(dd,max(dr,max(rd,rr)))+ans;
     }
     
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size();
-        if(grid[0][0]==-1 || grid[n-1][n-1]==-1)
-            return 0;
+        
         vector<vector<vector<vector<int>>>>dp(n,vector<vector<vector<int>>>(n,vector<vector<int>>(n,vector<int>(n,-1))));
-        int ans=helper(grid,0,0,0,0,n,dp);
+        int ans=helper(0,0,0,0,grid,n,dp);
         if(ans<0)
             return 0;
         return ans;
