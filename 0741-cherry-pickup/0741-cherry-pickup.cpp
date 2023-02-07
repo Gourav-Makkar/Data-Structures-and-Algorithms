d6e947746053b63d203e0f61739dbba92a4ee29c
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-02-07 23:45:16 +0530

    Time: 59 ms (76.61%), Space: 22.4 MB (66.42%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     
-    int helper(int r1,int c1,int r2,int c2,vector<vector<int>>&grid,int n,vector<vector<vector<vector<int>>>>&dp)
+    int helper(int r1,int c1,int c2,vector<vector<int>>&grid,int n,vector<vector<vector<int>>>&dp)
     {
+        int r2=r1+c1-c2;
         if(r1==n || c1==n || r2==n || c2==n || grid[r1][c1]==-1 || grid[r2][c2]==-1)
             return -1e9;
         
@@ -11,8 +12,8 @@ public:
         if(r2==n-1 && c2==n-1)
             return grid[r2][c2];
         
-        if(dp[r1][c1][r2][c2]!=-1)
-            return dp[r1][c1][r2][c2];
+        if(dp[r1][c1][c2]!=-1)
+            return dp[r1][c1][c2];
         
         int ans=0;
         
@@ -21,19 +22,19 @@ public:
         else
             ans=grid[r1][c1]+grid[r2][c2];
         
-        int dd=helper(r1+1,c1,r2+1,c2,grid,n,dp);
-        int dr=helper(r1+1,c1,r2,c2+1,grid,n,dp);
-        int rd=helper(r1,c1+1,r2+1,c2,grid,n,dp);
-        int rr=helper(r1,c1+1,r2,c2+1,grid,n,dp);
+        int dd=helper(r1+1,c1,c2,grid,n,dp);
+        int dr=helper(r1+1,c1,c2+1,grid,n,dp);
+        int rd=helper(r1,c1+1,c2,grid,n,dp);
+        int rr=helper(r1,c1+1,c2+1,grid,n,dp);
     
-        return dp[r1][c1][r2][c2]=max(dd,max(dr,max(rd,rr)))+ans;
+        return dp[r1][c1][c2]=max(dd,max(dr,max(rd,rr)))+ans;
     }
     
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size();
         
-        vector<vector<vector<vector<int>>>>dp(n,vector<vector<vector<int>>>(n,vector<vector<int>>(n,vector<int>(n,-1))));
-        int ans=helper(0,0,0,0,grid,n,dp);
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(n,vector<int>(n,-1)));
+        int ans=helper(0,0,0,grid,n,dp);
         if(ans<0)
             return 0;
         return ans;
