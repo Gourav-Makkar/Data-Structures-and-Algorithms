491de1ec0c307831b007e0794f36f2e68103da52
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-03-15 20:39:40 +0530

    Time: 545 ms (43.18%), Space: 10.4 MB (63.92%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,48 +1,30 @@
 class Solution {
 public:
     
-    int helper(int st,int en,vector<int>&v,vector<vector<int>>&dp)
+    int helper(int i,int j,vector<int>&nums,vector<vector<int>>&dp)
     {
-        if(st>en)
+        if(i>j)
             return 0;
         
-        if(dp[st][en]!=-1)
-            return dp[st][en];
-        int ans=INT_MIN;
-        for(int k=st;k<=en;k++)
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        int coins=INT_MIN;
+
+        for(int k=i;k<=j;k++)
         {
-            ans=max(ans,helper(st,k-1,v,dp) + v[st-1]*v[k]*v[en+1] + helper(k+1,en,v,dp));
+            coins=max(coins,helper(i,k-1,nums,dp) + nums[i-1]*nums[k]*nums[j+1] + helper(k+1,j,nums,dp));
         }
-        return dp[st][en]=ans;
+        
+        return dp[i][j]=coins;
     }
     
     int maxCoins(vector<int>& nums) {
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        
         int n=nums.size();
-        vector<int>v(n+2,1);
-        
-        for(int i=1;i<n+1;i++)
-            v[i]=nums[i-1];
-        
-        vector<vector<int>>dp(n+2,vector<int>(n+2,-1));
-        return helper(1,n,v,dp);
-        
-//         for(int i=1;i<=n;i++)
-//         {
-//             dp[i][i]=v[i-1]*v[i]*v[i+1];
-//         }
-        
-//         for(int length=2;length<=n;length++)
-//         {
-//             for(int i=1;i<=n-length+1;i++)
-//             {
-//                 int j=i+length-1;
-                
-//                 for(int k=i;k<=j;k++)
-//                 {
-//                     dp[i][j]=max(dp[i][j],dp[i][k-1]+dp[k+1][j]+ v[i-1]*v[k]*v[j+1]);
-//                 }
-//             }
-//         }
-//         return dp[1][n];
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return helper(1,n-2,nums,dp);
     }
 };
