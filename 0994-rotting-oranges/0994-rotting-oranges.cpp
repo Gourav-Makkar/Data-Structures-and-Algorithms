0697d4083b4ac2c96fd3c790cae5a8d2f0dceca5
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-03-16 17:54:43 +0530

    Time: 11 ms (41.33%), Space: 13.6 MB (15.67%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,72 +1,62 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int r=grid.size();
-        int c=grid[0].size();
-        int mins=0;
         queue<pair<int,int>>q;
+        int n=grid.size();
+        int m=grid[0].size();
+        int t=0;
         
-        for(int i=0;i<r;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<c;j++)
+            for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==2)
-                  q.push({i,j});
+                {
+                    q.push({i,j});
+                }
             }
         }
         
         while(!q.empty())
         {
-            int n=q.size();
             bool flag=false;
-            for(int i=0;i<n;i++)
+            int s=q.size();
+            
+            for(int i=0;i<s;i++)
             {
                 int cr=q.front().first;
                 int cc=q.front().second;
                 q.pop();
                 
-                if(cr>0 && grid[cr-1][cc]==1)
-              {
-                flag=true;
-                grid[cr-1][cc]=2;
-                q.push({cr-1,cc});
-              }
-            
-              if(cc<c-1 && grid[cr][cc+1]==1)
-              {
-                flag=true;
-                grid[cr][cc+1]=2;
-                q.push({cr,cc+1});
-              }
-            
-              if(cr<r-1 && grid[cr+1][cc]==1)
-              {
-                flag=true;
-                grid[cr+1][cc]=2;
-                q.push({cr+1,cc});
-              }
-            
-              if(cc>0 && grid[cr][cc-1]==1)
-              {
-                flag=true;
-                grid[cr][cc-1]=2;
-                q.push({cr,cc-1});
-              }
+                vector<int>row{-1,0,1,0},col{0,-1,0,1};
                 
+                for(int i=0;i<4;i++)
+                {
+                    int nr=cr+row[i];
+                    int nc=cc+col[i];
+                    
+                    if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1)
+                    {
+                        q.push({nr,nc});
+                        grid[nr][nc]=2;
+                        flag=true;
+                    }
+                }
             }
-            
             if(flag)
-                mins++;
+               t++;
         }
         
-        for(int i=0;i<r;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<c;j++)
+            for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==1)
                     return -1;
             }
         }
-        return mins;
+
+        return t;
     }
+    
 };
