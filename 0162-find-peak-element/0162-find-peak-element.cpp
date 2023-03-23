199e9df048268ecfd05b26aa88fa3d8b1bd71ec1
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-03-23 13:41:26 +0530

    Time: 3 ms (86.08%), Space: 9 MB (31.74%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -9,29 +9,12 @@ public:
         while(st<=en)
         {
             int mid=st+(en-st)/2;
-            if(mid==0)
-            {
-                if(nums[mid]>nums[mid+1])
-                    return mid;
-                else
-                    st=mid+1;
-            }
-            else if(mid==n-1)
-            {
-                if(nums[mid]>nums[mid-1])
-                    return mid;
-                else
-                    en=mid-1;
-            }
-            else
-            {
-                if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
-                    return mid;
-                else if(nums[mid]<nums[mid-1])
-                    en=mid-1;
-                else if(nums[mid]<nums[mid+1])
-                    st=mid+1;
-            }
+            if((mid==0 || nums[mid]>nums[mid-1]) && (mid==n-1 || nums[mid]>nums[mid+1]))
+                return mid;
+            else if(nums[mid]<nums[mid+1])
+                st=mid+1;
+            else if(nums[mid]<nums[mid-1])
+                en=mid-1;
         }
         return -1;
     }
