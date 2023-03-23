5fb9b4db15f6c2a4ae87dda70e5ed87234706cf3
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-03-23 10:55:31 +0530

    Time: 156 ms (39.67%), Space: 55.3 MB (27.94%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -4,37 +4,35 @@ public:
     void dfs(int curr,vector<int>adj[],vector<int>&vis)
     {
         vis[curr]=1;
-        
         for(auto it:adj[curr])
         {
-            if(vis[it]==0)
+            if(!vis[it])
                 dfs(it,adj,vis);
         }
     }
     
-    int makeConnected(int n, vector<vector<int>>& con) {
-        vector<int>adj[n];
-        
-        if(con.size()<n-1)
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size()<n-1)
             return -1;
         
-        for(auto it:con)
+        vector<int>adj[n];
+        for(auto it:connections)
         {
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
-        vector<int>vis(n,0);
-        int c=0;
         
+        int comp=0;
+        
+        vector<int>vis(n,0);
         for(int i=0;i<n;i++)
         {
-            if(vis[i]==0)
+            if(!vis[i])
             {
-                c++;
+                comp++;
                 dfs(i,adj,vis);
             }
         }
-       
-        return c-1;
+        return comp-1;
     }
 };
