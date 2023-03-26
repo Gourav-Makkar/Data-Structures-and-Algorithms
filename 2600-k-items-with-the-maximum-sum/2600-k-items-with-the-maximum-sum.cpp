d9800bdc4cfcf324ca75a9a5f3c522829f48b2f3
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-03-26 13:10:27 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (88.89%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        int ans=0;
+        
+        while(k--)
+        {
+            if(numOnes!=0)
+            {
+                ans+=1;
+                numOnes--;
+            }
+            else if(numZeros!=0)
+              numZeros--;
+            else
+            {
+                ans-=1;
+                numNegOnes--;
+            }
+        }
+        return ans;
+    }
+};
