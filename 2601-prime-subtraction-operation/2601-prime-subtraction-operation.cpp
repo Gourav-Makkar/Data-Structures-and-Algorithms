cf20a33b153654bf4facd64bc06f2d0014454da1
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-03-26 13:29:38 +0530

    Time: 22 ms (94.12%), Space: 23.5 MB (88.24%) - LeetHub

---
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    bool checkPrime(int n)
+    {
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    
+    int findPrime(int n)
+    {
+        while(n>1)
+        {
+            if(checkPrime(n))
+                return n;
+            n--;
+        }
+        return -1;
+    }
+    
+    bool primeSubOperation(vector<int>& nums) {
+        int n=nums.size();
+        int prev=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(i==n-1 && nums[i]>prev)
+                break;
+            
+            int val=findPrime(nums[i]-prev-1);
+            if(val==-1)
+            {
+                if(nums[i]>prev)
+                  prev=nums[i];
+                else
+                    return false;
+            }
+            else
+              prev=nums[i]-val;
+        }
+        return true;
+    }
+};
