c2cf184b69b074ed8395f8458d4f1882fb52b5da
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-03-27 11:05:45 +0530

    Time: 15 ms (20.14%), Space: 10.6 MB (7.26%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,50 +1,32 @@
 class Solution {
 public:
     
-    int helper(int cr,int cc,vector<vector<int>>&grid,int n,int m,vector<vector<int>>&dp)
+    int helper(int cr,int cc,vector<vector<int>>&grid,vector<vector<int>>&vis,vector<vector<int>>&dp)
     {
-        if(cr==n || cr<0 || cc<0 || cc==m)
-            return 1e9;
+        int n=grid.size(),m=grid[0].size();
         
+        if(cr==n || cc==m || vis[cr][cc])
+            return 1e9;
         if(cr==n-1 && cc==m-1)
             return grid[cr][cc];
         
         if(dp[cr][cc]!=-1)
             return dp[cr][cc];
         
-        int down=grid[cr][cc]+helper(cr+1,cc,grid,n,m,dp);
-        int right=grid[cr][cc]+helper(cr,cc+1,grid,n,m,dp);
+        vis[cr][cc]=1;
         
+        int down=grid[cr][cc]+helper(cr+1,cc,grid,vis,dp);
+        int right=grid[cr][cc]+helper(cr,cc+1,grid,vis,dp);
+        
+        vis[cr][cc]=0;
         return dp[cr][cc]=min(down,right);
     }
     
     int minPathSum(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
         vector<vector<int>>dp(n,vector<int>(m,-1));
         
-        // return helper(0,0,grid,n,m,dp);
-        int cs=0;
-        
-        for(int i=0;i<n;i++)
-        {
-            cs+=grid[i][0];
-            dp[i][0]=cs;
-        }
-        cs=0;
-        for(int i=0;i<m;i++)
-        {
-            cs+=grid[0][i];
-            dp[0][i]=cs;
-        }
-        for(int i=1;i<n;i++)
-        {
-            for(int j=1;j<m;j++)
-            {
-                dp[i][j]=min(dp[i-1][j],dp[i][j-1]) + grid[i][j];
-            }
-        }
-         return dp[n-1][m-1];   
+        return helper(0,0,grid,vis,dp);
     }
 };
