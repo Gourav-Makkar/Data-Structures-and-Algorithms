c201f9faf622b8318565824878d73055453e040f
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-03-28 17:59:22 +0530

    Time: 6 ms (40.86%), Space: 9.7 MB (57.33%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     
-    int helper(int idx,vector<int>&days,vector<int>&costs,int avail,vector<vector<int>>&dp)
+    int helper(int idx,vector<int>&days,vector<int>&costs,vector<int>&dp)
     {
         if(idx==days.size())
             return 0;
         
-        if(dp[idx][avail]!=-1)
-            return dp[idx][avail];
+        if(dp[idx]!=-1)
+            return dp[idx];
         
-        if(days[idx]<=avail)
-            return dp[idx][avail]=helper(idx+1,days,costs,avail,dp);
+        int idx1=upper_bound(days.begin(),days.end(),days[idx]+6)-days.begin();
+        int idx2=upper_bound(days.begin(),days.end(),days[idx]+29)-days.begin();
         
-        int first=costs[0]+helper(idx+1,days,costs,days[idx],dp);
-        int second=costs[1]+helper(idx+1,days,costs,days[idx]+7-1,dp);
-        int third=costs[2]+helper(idx+1,days,costs,days[idx]+30-1,dp);
+        int first=costs[0]+helper(idx+1,days,costs,dp);
+        int second=costs[1]+helper(idx1,days,costs,dp);
+        int third=costs[2]+helper(idx2,days,costs,dp);
         
-        return dp[idx][avail]=min(first,min(second,third));
+        return dp[idx]=min(first,min(second,third));
     }
     
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<vector<int>>dp(days.size(),vector<int>(396,-1));
-        return helper(0,days,costs,0,dp);
+        vector<int>dp(days.size(),-1);
+        return helper(0,days,costs,dp);
     }
 };
