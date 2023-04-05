c4d3597e0e97a6ce574c73b9dbb15828eed0253f
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-04-05 17:17:41 +0530

    Time: 439 ms (5.02%), Space: 361.8 MB (5.02%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,37 +1,39 @@
-typedef long long int ll;
 class Solution {
 public:
     
-    bool check(ll mx,vector<int>nums)
+    bool check(long long mid,vector<int>temp)
     {
-        vector<ll>arr(nums.begin(),nums.end());
-        for(int i=0;i<arr.size()-1;i++)
-        {
-           if(arr[i]>mx)
-               return false;
-            arr[i+1]-=(mx-arr[i]);
-        }
-        if(arr[arr.size()-1]<=mx)
-            return true;
-        else
-            return false;
+        vector<long long>nums;
+        for(auto it:temp)
+            nums.push_back(it);
         
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i-1]<mid)
+            {
+                long long ct=mid-nums[i-1];
+                nums[i-1]+=ct;
+                nums[i]-=ct;
+            }
+            else if(nums[i-1]>mid)
+                return false;
+        }
+        return nums[nums.size()-1]<=mid;
     }
     
     int minimizeArrayValue(vector<int>& nums) {
-        int st=INT_MAX;
-        int en=INT_MIN;
         int n=nums.size();
-        int ans;
-        for(int i=0;i<n;i++)
-        {
-            st=min(st,nums[i]);
-            en=max(en,nums[i]);
-        }
+        int st=0,en=INT_MIN,ans;
+        
+        for(auto it:nums)
+            en=max(it,en);
         
         while(st<=en)
         {
             int mid=st+(en-st)/2;
+            
+            // cout<<st<<" "<<en<<endl;
+            
             if(check(mid,nums))
             {
                 ans=mid;
