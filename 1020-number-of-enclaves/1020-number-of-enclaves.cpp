f5707a7408da5199b8b08c94e8ad0fdead36a683
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-04-07 12:21:34 +0530

    Time: 67 ms (93.69%), Space: 33 MB (28.03%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,63 +1,53 @@
 class Solution {
 public:
     
-    void dfs(vector<vector<int>>&grid,int cr,int cc,int r,int c,vector<vector<int>>&vis)
+    void dfs(int cr,int cc,vector<vector<int>>&grid,vector<vector<int>>&vis)
     {
-        if(cr<0 || cr==r || cc<0 || cc==c || grid[cr][cc]==0 || vis[cr][cc]==1)
-            return;
+        int n=grid.size(),m=grid[0].size();
+        
+        if(cr<0 || cr==n || cc<0 || cc==m || grid[cr][cc]==0 || vis[cr][cc]==1)
+            return ;
         
         vis[cr][cc]=1;
         
-        dfs(grid,cr-1,cc,r,c,vis);
-        dfs(grid,cr,cc+1,r,c,vis);
-        dfs(grid,cr+1,cc,r,c,vis);
-        dfs(grid,cr,cc-1,r,c,vis);
+        dfs(cr-1,cc,grid,vis);
+        dfs(cr,cc+1,grid,vis);
+        dfs(cr+1,cc,grid,vis);
+        dfs(cr,cc-1,grid,vis);
     }
     
     int numEnclaves(vector<vector<int>>& grid) {
-        int r=grid.size();
-        int c=grid[0].size();
-        int ans=0;
+        int n=grid.size(),m=grid[0].size();
         
-        vector<vector<int>>vis(r,vector<int>(c,0));
+        vector<vector<int>>vis(n,vector<int>(m,0));
         
-        // First Row
-        for(int i=0;i<c;i++)
-        {
-            if(grid[0][i]==1)
-                dfs(grid,0,i,r,c,vis);
-        }
-        
-        //Last Column
-        for(int i=0;i<r;i++)
-        {
-            if(grid[i][c-1]==1)
-                dfs(grid,i,c-1,r,c,vis);
-        }
-        
-        //Last Row
-        for(int i=0;i<c;i++)
-        {
-            if(grid[r-1][i]==1)
-                dfs(grid,r-1,i,r,c,vis);
-        }
-        
-        //First Column
-        for(int i=0;i<r;i++)
+        for(int i=0;i<n;i++)
         {
             if(grid[i][0]==1)
-                dfs(grid,i,0,r,c,vis);
+                dfs(i,0,grid,vis);
+            
+            if(grid[i][m-1]==1)
+                dfs(i,m-1,grid,vis);
         }
         
-        for(int i=0;i<r;i++)
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<c;j++)
+            if(grid[0][i]==1)
+                dfs(0,i,grid,vis);
+            
+            if(grid[n-1][i]==1)
+                dfs(n-1,i,grid,vis);
+        }
+        
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==1 && vis[i][j]==0)
                     ans++;
             }
         }
-        
         return ans;
     }
 };
