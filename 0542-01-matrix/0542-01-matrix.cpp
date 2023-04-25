4735455db9b172279f396b0c9f3b950ee6846118
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-04-25 13:32:23 +0530

    Time: 221 ms (6.76%), Space: 59.6 MB (5.08%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,61 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int r=mat.size();
-        int c=mat[0].size();
+        int n=mat.size(),m=mat[0].size();
+        vector<vector<int>>ans(n,vector<int>(m,0));
         
-        vector<vector<int>>ans(r,vector<int>(c));
-        vector<vector<int>>vis(r,vector<int>(c,0));
-         
-        queue<pair<pair<int,int>,int>>q;
-        
-        for(int i=0;i<r;i++)
+        queue<vector<int>>q;
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<c;j++)
+            for(int j=0;j<m;j++)
             {
                 if(mat[i][j]==0)
                 {
-                    q.push({{i,j},0});
-                    vis[i][j]=1;
+                    q.push({i,j,0});
+                    mat[i][j]=-1;
                 }
             }
         }
         
         while(!q.empty())
         {
-            int cr=q.front().first.first;
-            int cc=q.front().first.second;
+            int cr=q.front()[0];
+            int cc=q.front()[1];
             
-            int dis=q.front().second;
+            // cout<<cr<<" "<<cc<<endl;
+            
+            int dis=q.front()[2];
             q.pop();
+            
             ans[cr][cc]=dis;
             
-            if(cr>0 && vis[cr-1][cc]!=1)
-            {
-                q.push({{cr-1,cc},dis+1});
-                vis[cr-1][cc]=1;
-            }
+            vector<int>temp={-1,0,1,0,-1};
             
-            if(cc<c-1 && vis[cr][cc+1]!=1)
+            for(int i=1;i<5;i++)
             {
-                q.push({{cr,cc+1},dis+1});
-                vis[cr][cc+1]=1;
-            }
-            
-            if(cr<r-1 && vis[cr+1][cc]!=1)
-            {
-                 q.push({{cr+1,cc},dis+1});
-                 vis[cr+1][cc]=1;
-            }
-            
-            if(cc>0 && vis[cr][cc-1]!=1)
-            {
-                q.push({{cr,cc-1},dis+1});
-                vis[cr][cc-1]=1;
+                int nr=cr+temp[i-1];
+                int nc=cc+temp[i];
+                
+                if(nr>=0 && nr<n && nc>=0 && nc<m && mat[nr][nc]==1)
+                {
+                    q.push({nr,nc,dis+1});
+                    mat[nr][nc]=-1;
+                }
             }
         }
-        
         return ans;
-        
     }
 };
