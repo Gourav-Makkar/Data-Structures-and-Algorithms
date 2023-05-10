2cdef6fdd9a844b5e7b5bce82b33fc45524f2e9e
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-05-10 19:56:27 +0530

    Time: 221 ms (6.67%), Space: 59.8 MB (5.08%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size(),m=mat[0].size();
-        vector<vector<int>>ans(n,vector<int>(m,0));
-        
+        vector<vector<int>>ans(n,vector<int>(m,INT_MAX));
         queue<vector<int>>q;
+        
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(mat[i][j]==0)
                 {
-                    q.push({i,j,0});
                     mat[i][j]=-1;
+                    q.push({i,j,0});
                 }
             }
         }
@@ -21,20 +21,16 @@ public:
         {
             int cr=q.front()[0];
             int cc=q.front()[1];
-            
-            // cout<<cr<<" "<<cc<<endl;
-            
             int dis=q.front()[2];
             q.pop();
             
             ans[cr][cc]=dis;
-            
+        
             vector<int>temp={-1,0,1,0,-1};
-            
             for(int i=1;i<5;i++)
             {
-                int nr=cr+temp[i-1];
-                int nc=cc+temp[i];
+                int nr=cr+temp[i];
+                int nc=cc+temp[i-1];
                 
                 if(nr>=0 && nr<n && nc>=0 && nc<m && mat[nr][nc]!=-1)
                 {
