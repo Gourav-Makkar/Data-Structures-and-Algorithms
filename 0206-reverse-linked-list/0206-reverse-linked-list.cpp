e9dea0c892258f7a9117ff7c0a6c974c72da2359
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-05-28 20:32:17 +0530

    Time: 4 ms (71.42%), Space: 8.6 MB (7.97%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -24,6 +24,13 @@ public:
     }
     
     ListNode* reverseList(ListNode* head) {
-        return reverse(NULL,head,NULL);
+       if(head==NULL || head->next==NULL)
+           return head;
+        
+       ListNode* newHead=reverseList(head->next);
+        
+       head->next->next=head;
+       head->next=NULL;
+       return newHead;
     }
 };
