e45b47728962e3052d170fec988af53c9ac6d092
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-05-28 12:25:01 +0530

    Time: 14 ms (75.76%), Space: 13.2 MB (88.05%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,30 +3,37 @@ public:
     void setZeroes(vector<vector<int>>& mat) {
         int n=mat.size(),m=mat[0].size();
         
-        vector<vector<int>>vis(n,vector<int>(m,0));
+        int col=1;
+        
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(mat[i][j]==0)
-                    vis[i][j]=1;
-            }
-        }
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(vis[i][j])
                 {
-                    for(int r=0;r<n;r++)
-                        mat[r][j]=0;
+                    mat[i][0]=0;
                     
-                    for(int c=0;c<m;c++)
-                        mat[i][c]=0;
+                    if(j!=0)
+                        mat[0][j]=0;
+                    else
+                        col=0;
                 }
             }
         }
         
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=1;j--)
+            {
+                if(mat[i][0]==0 || mat[0][j]==0)
+                    mat[i][j]=0;
+            }
+        }
+        
+        if(!col)
+        {
+            for(int i=0;i<n;i++)
+                mat[i][0]=0;
+        }
     }
 };
