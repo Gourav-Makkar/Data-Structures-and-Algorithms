888f94cfa24982b42be33a82a24d6907b9b361a1
Gourav Makkar <87635847+Gourav-Makkar@users.noreply.github.com>
2023-06-03 12:09:33 +0530

    Time: 363 ms (35.15%), Space: 117.6 MB (69.14%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -7,14 +7,11 @@ public:
         {
             if(manager[i]!=-1)
             {
-                adj[i].push_back(manager[i]);
                 adj[manager[i]].push_back(i);
             }
         }
         
         int ans=0;
-        vector<int>vis(n,0);
-        vis[headId]=1;
         queue<pair<int,int>>q;
         q.push({headId,0});
         
@@ -28,11 +25,7 @@ public:
             ans=max(ans,time);
             for(auto it:adj[curr])
             {
-                if(!vis[it])
-                {
-                    vis[it]=1;
-                    q.push({it,time+inform[curr]});
-                }
+                q.push({it,time+inform[curr]});
             }
         }
         return ans;
